810bbddec8cfb171d2aa2a16a53aaf67bae3677a
Yesh <sriyesh1999@gmail.com>
2024-07-11 06:04:30 -0400

    frontend employee view and nav development

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'http://127.0.0.1:5000';
+  private apiUrl = 'http://127.0.0.1:8080';
 
   constructor(private http: HttpClient) { }
 
