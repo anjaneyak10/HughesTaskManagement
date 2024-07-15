62df9bf3732eb7816b951547df54b75cf7007dac
Yesh <sriyesh1999@gmail.com>
2024-07-15 06:02:25 -0400

    backend api invoking to display dynamic data on frontend

--- src/app/components/project-view/project-view.component.ts
+++ src/app/components/project-view/project-view.component.ts
@@ -8,7 +8,6 @@ interface Project {
   assignee: string;
   specialInstructions: string;
   exceptions: string;
-  dependentTasks: number;
 }
 interface ProjectSelect{
   name: string;
@@ -22,7 +21,7 @@ interface ProjectSelect{
   { task: 'Something Else', completion: '80%', assignee: '', specialInstructions: '', exceptions: '', dependentTasks: 123 },
   { task: 'Obtain Final Site List', completion: '', assignee: 'Nike', specialInstructions: 'Something special', exceptions: 'Some Exception', dependentTasks: 1 }
 ];*/
-const PROJECT_DATA: Project[] = [];
+var PROJECT_DATA: Project[] = [];
 const projectList: ProjectSelect[] = []
 
 @Component({
@@ -31,37 +30,45 @@ const projectList: ProjectSelect[] = []
   styleUrls: ['./project-view.component.css']
 })
 export class ProjectViewComponent implements OnInit {
-  displayedColumns: string[] = ['task', 'completion', 'assignee', 'specialInstructions', 'exceptions', 'dependentTasks'];
-  dataSource = new MatTableDataSource(PROJECT_DATA);
+  displayedColumns: string[] = ['task', 'completion', 'assignee', 'specialInstructions', 'exceptions'];
+  dataSource: MatTableDataSource<Project>;
   projectNames: string[] =[];
   spinner = true;
+  spinnerTable=false;
   constructor(private taskService: TaskService) {
-
+    this.dataSource = new MatTableDataSource<Project>([]);
   }
 
   ngOnInit(): void {
     this.taskService.getProjectList('sst@gmail.com').subscribe(tasks => {
-      const uniqueProjectNames: Set<string> = new Set(); // Use a Set to store unique names
+      console.log(JSON.stringify(tasks));
       tasks.forEach((proj: any) => {
-        projectList.push({name:proj[1],projectId:proj[0]}); // Add project name to the Set
+        projectList.push({name:proj.projectname,projectId:proj.projectid}); // Add project name to the Set
       });
       this.projectNames = Array.from(new Set(projectList.map(project => project.name))); // Convert Set back to array of project names
+      console.log(this.projectNames);
       this.spinner = false;
     });
   }
 
   applyFilter(filterValue: string): void {
-    this.spinner = true;
+    this.spinnerTable = true;
     for(let i in projectList){
       if(projectList[i].name == filterValue){
         filterValue = projectList[i].projectId;
       }
     }
     this.taskService.getProjectInfo(filterValue).subscribe(projectInfo => {
-      console.log(projectInfo);
-      this.spinner = false;
+      console.log(filterValue);
+      this.dataSource = new MatTableDataSource<Project>([]);
+      this.spinnerTable = true;
+      PROJECT_DATA=[]
+      projectInfo.forEach((i: any) => {
+      PROJECT_DATA.push({ task: i.taskname, completion: i.completion, assignee: i.assignee, specialInstructions: i.specialinstructions, exceptions: i.exceptions});
+      });
+      console.log(PROJECT_DATA)
+      this.dataSource = new MatTableDataSource(PROJECT_DATA);
+      this.spinnerTable = false;
     });
-
-    
   }
 }
