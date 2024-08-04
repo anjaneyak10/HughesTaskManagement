52a81e47cd689a9b6b3af6c3d112f2cab657bfeb
dhruvin18 <dhruvin30@somaiya.edu>
2024-08-04 11:34:54 -0400

    Add grouping to Template

--- src/app/components/select-tasks/select-tasks.component.ts
+++ src/app/components/select-tasks/select-tasks.component.ts
@@ -2,15 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TemplateService } from 'src/app/services/template.service';
 
+interface Task {
+  createdby: string;
+  createdbyname: string;
+  createdon: string;
+  function_id: string;
+  function_name: string;
+  lastmodifiedby: string | null;
+  lastmodifiedbyname: string | null;
+  lastmodifiedon: string | null;
+  taskid: string;
+  taskname: string;
+  weightage: string;
+  selected: boolean;
+}
+
 @Component({
   selector: 'app-select-tasks',
   templateUrl: './select-tasks.component.html',
   styleUrls: ['./select-tasks.component.css']
 })
 export class SelectTasksComponent implements OnInit {
-  tasks: any[] = [];
-  source: any[] = [];
-  confirmed: any[] = [];
+  tasks: Task[] = [];
+  groupedSource: { [key: string]: Task[] } = {};
+  groupedConfirmed: { [key: string]: Task[] } = {};
   templateId: string | null = '';
   showConfirmation: boolean = false;
 
@@ -24,38 +39,56 @@ export class SelectTasksComponent implements OnInit {
     this.templateId = this.route.snapshot.paramMap.get('id');
     this.templateService.getAllTasks().subscribe(
       response => {
-        this.tasks = response.tasks.map((task: any) => ({ ...task, selected: false }));
-        this.source = [...this.tasks];
+        this.tasks = response.tasks.map((task: Task) => ({ ...task, selected: false }));
+        this.groupedSource = this.groupTasksByFunction(this.tasks);
+        this.groupedConfirmed = {};
       },
       error => console.error('Error fetching tasks:', error)
     );
   }
 
-  addTask(task: any) {
-    task.selected = true;
-    this.confirmed.push(task);
-    this.source = this.source.filter(t => t.taskid !== task.taskid);
+  groupTasksByFunction(tasks: Task[]): { [key: string]: Task[] } {
+    const groupedTasks: { [key: string]: Task[] } = {};
+    tasks.forEach(task => {
+      if (!groupedTasks[task.function_name]) {
+        groupedTasks[task.function_name] = [];
+      }
+      groupedTasks[task.function_name].push(task);
+    });
+    return groupedTasks;
   }
 
-  removeTask(task: any) {
+  addTask(task: Task) {
+    task.selected = true;
+    if (!this.groupedConfirmed[task.function_name]) {
+      this.groupedConfirmed[task.function_name] = [];
+    }
+    this.groupedConfirmed[task.function_name].push(task);
+    this.groupedSource[task.function_name] = this.groupedSource[task.function_name].filter((t: Task) => t.taskid !== task.taskid);
+  }
+
+  removeTask(task: Task) {
     task.selected = false;
-    this.source.push(task);
-    this.confirmed = this.confirmed.filter(t => t.taskid !== task.taskid);
+    if (!this.groupedSource[task.function_name]) {
+      this.groupedSource[task.function_name] = [];
+    }
+    this.groupedSource[task.function_name].push(task);
+    this.groupedConfirmed[task.function_name] = this.groupedConfirmed[task.function_name].filter((t: Task) => t.taskid !== task.taskid);
   }
 
   addAllTasks() {
-    this.confirmed = this.tasks.map(task => ({ ...task, selected: true }));
-    this.source = [];
+    this.groupedConfirmed = this.groupTasksByFunction(this.tasks.map((task: Task) => ({ ...task, selected: true })));
+    this.groupedSource = {};
   }
 
   removeAllTasks() {
-    this.source = this.tasks.map(task => ({ ...task, selected: false }));
-    this.confirmed = [];
+    this.groupedSource = this.groupTasksByFunction(this.tasks.map((task: Task) => ({ ...task, selected: false })));
+    this.groupedConfirmed = {};
   }
 
   addTasksToTemplate() {
     if (this.templateId) {
-      const selectedTaskIds = this.confirmed.map(task => task.taskid);
+      const selectedTaskIds = Object.values(this.groupedConfirmed).flat().map((task: Task) => task.taskid);
 
       if (selectedTaskIds.length > 0) {
         this.templateService.addTasksToTemplate(this.templateId, selectedTaskIds).subscribe(
